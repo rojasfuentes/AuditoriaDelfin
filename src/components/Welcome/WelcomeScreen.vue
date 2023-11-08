2dc2a2ad9b616750b5d8c91d53619b7d57d4ced8
AlvirElWapo <correodeandresalvir@gmail.com>
2023-11-08 15:32:48 -0600

    modifications

--- src/components/Welcome/WelcomeScreen.vue
+++ src/components/Welcome/WelcomeScreen.vue
@@ -3,10 +3,11 @@
     <div class="welcome_container__main">
       <div class="main__buttons">
         <h1 class="main__buttons___title">Bienvenido</h1>
-        <button class="btn main__buttons___btn1">
+        <button class="btn main__buttons___btn1" @click="showHiddenComponent = true">
           <font-awesome-icon class="icon" icon="fa-solid fa-flag-checkered" />
-          Ingresar</button
-        ><button class="btn main__buttons___btn2">
+          Ingresar
+        </button>
+        <button class="btn main__buttons___btn2">
           <font-awesome-icon class="icon" icon="fa-solid fa-flag-checkered" />
           Lorem, ipsum.
         </button>
@@ -17,15 +18,74 @@
         alt="logo"
       />
     </div>
+    <div class="floating-login-form" v-if="showHiddenComponent">
+      
+      <h5 style="font-size: 18px;">INICIAR SESIÓN</h5>
+      <form id="loginForm" action="your-login-action.php" method="post">
+        <div class="form-group">
+          <label for="username">Usuario:</label>
+          <input type="text" id="username" name="username" class="form-control" required placeholder="Usuario">
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" name="password" class="form-control" required placeholder="Contraseña">
+        </div>
+        <button type="submit" class="btn btn-primary">Entrar</button>
+      </form>
+    </div>
   </div>
-  <div class="welcome_container_brands"></div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showHiddenComponent: false, // Initially hidden
+    };
+  },
+};
 </script>
 
+
+
 <style scoped>
+
+.hidden-component {
+  display: block; /* Initially shown */
+}
+
+.floating-login-form {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  z-index: 9999; /* Ensures it appears on top of everything else */
+}
+
+.section-title{}
+.section-title h5{
+    color: #005289;
+    position: relative;
+    padding-bottom: 12px; margin-bottom:15px; font-size: 30px;
+}
+.section-title h5::before{
+    content: '';
+    position: absolute;
+    width: 35px;
+    height: 2px;
+    background-color: #f48120;
+    bottom: 0;
+    left: 0;
+}
+.section-title h2{
+    font-size: 38px;
+    padding-top: 10px;
+}
+
 .welcome_container {
   position: relative;
   width: 100vw;
@@ -91,4 +151,5 @@ export default {};
   width: 100vw;
   height: 20vh;
 }
+
 </style>
